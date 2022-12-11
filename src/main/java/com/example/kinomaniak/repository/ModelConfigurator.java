b867edd2e9665d844a3cd7208a1a54cdca69ba44
pwgacek <pawelgacek.pl@gmail.com>
2022-12-11 12:06:35 +0100

    changed model: Movie and FilmShow
    Now movie can have multiple categories, so new link table (movie_categories) was created in database.
    Added new columns to FilmShow table: with_subtitles and is3d, both with boolean values

--- src/main/java/com/example/kinomaniak/repository/ModelConfigurator.java
+++ src/main/java/com/example/kinomaniak/repository/ModelConfigurator.java
@@ -3,9 +3,11 @@ package com.example.kinomaniak.repository;
 import com.example.kinomaniak.model.FilmShow;
 import com.example.kinomaniak.model.Hall;
 import com.example.kinomaniak.model.Movie;
+import com.example.kinomaniak.model.MovieCategory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.ZonedDateTime;
 import java.util.Optional;
@@ -26,11 +28,14 @@ public class ModelConfigurator {
                 Optional<Movie> movieOptional = movieRepository.findMovieByTitle("Inception");
                 Optional<Hall> hallOptional = hallRepository.findHallByHallNo(1);
                 if(movieOptional.isPresent() && hallOptional.isPresent()){
-                    FilmShow show = new FilmShow(hallOptional.get(),movieOptional.get(), ZonedDateTime.now(),12.99);
+                    FilmShow show = new FilmShow(hallOptional.get(),movieOptional.get(), ZonedDateTime.now(),12.99,true,false);
                     filmShowRepository.save(show);
                     System.out.println("found");
                 }
             }
+            Optional<Movie> movieOptional = movieRepository.findMovieByTitle("Schindler's List");
+            movieOptional.ifPresent(movie -> movie.getCategories().stream().map(MovieCategory::getCategoryName).forEach(System.out::println));
+
 
         };
     }
