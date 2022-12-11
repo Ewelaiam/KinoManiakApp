fbf721b2054009d2afffece35d5f9a892f7cf341
pwgacek <pawelgacek.pl@gmail.com>
2022-12-11 12:12:56 +0100

    removed some test lines

--- src/main/java/com/example/kinomaniak/repository/ModelConfigurator.java
+++ src/main/java/com/example/kinomaniak/repository/ModelConfigurator.java
@@ -33,8 +33,8 @@ public class ModelConfigurator {
                     System.out.println("found");
                 }
             }
-            Optional<Movie> movieOptional = movieRepository.findMovieByTitle("Schindler's List");
-            movieOptional.ifPresent(movie -> movie.getCategories().stream().map(MovieCategory::getCategoryName).forEach(System.out::println));
+//            Optional<Movie> movieOptional = movieRepository.findMovieByTitle("Schindler's List");
+//            movieOptional.ifPresent(movie -> movie.getCategories().stream().map(MovieCategory::getCategoryName).forEach(System.out::println));
 
 
         };
