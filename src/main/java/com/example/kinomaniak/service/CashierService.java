f39c3e93fd46240e3307d62d67befc29676a1c27
Paweł Gacek <407085@student.agh.edu.pl>
2022-12-22 19:47:20 +0100

    final changes - bug fixed

--- src/main/java/com/example/kinomaniak/service/CashierService.java
+++ src/main/java/com/example/kinomaniak/service/CashierService.java
@@ -1,12 +1,12 @@
 package com.example.kinomaniak.service;
 
-import com.example.kinomaniak.model.Employee;
-import com.example.kinomaniak.model.FilmShow;
-import com.example.kinomaniak.model.Movie;
-import com.example.kinomaniak.model.Ticket;
+import com.example.kinomaniak.model.*;
 import com.example.kinomaniak.repository.EmployeeRepository;
+import com.example.kinomaniak.repository.MovieCategoryRepository;
 import com.example.kinomaniak.repository.MovieRepository;
 import com.example.kinomaniak.repository.TicketRepository;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
@@ -21,11 +21,14 @@ public class CashierService {
     private final EmployeeRepository employeeRepository;
 
     private final MovieRepository movieRepository;
+    private final MovieCategoryRepository movieCategoryRepository;
 
-    public CashierService(TicketRepository ticketRepository, EmployeeRepository employeeRepository, MovieRepository movieRepository) {
+    public CashierService(TicketRepository ticketRepository, EmployeeRepository employeeRepository,
+                          MovieRepository movieRepository, MovieCategoryRepository movieCategoryRepository) {
         this.ticketRepository = ticketRepository;
         this.employeeRepository = employeeRepository;
         this.movieRepository = movieRepository;
+        this.movieCategoryRepository = movieCategoryRepository;
     }
 
     public void changePassword(String mail, String password){
@@ -44,4 +47,12 @@ public class CashierService {
         return Optional.of(page.getContent());
     }
 
+    public ObservableList<Movie> getMovies(){
+        return FXCollections.observableList(movieRepository.findAll());
+    }
+
+
+    public List<MovieCategory> getMovieCategories() {
+        return movieCategoryRepository.findAll();
+    }
 }
