14a4d2538bdd5df0d456c55a3f8b05e873d4f1d6
Paweł Gacek <407085@student.agh.edu.pl>
2022-12-22 19:47:20 +0100

    final changes - bug fixed

---
+++ src/main/java/com/example/kinomaniak/controller/CashierMovieViewController.java
@@ -0,0 +1,166 @@
+package com.example.kinomaniak.controller;
+
+import com.example.kinomaniak.model.Movie;
+import com.example.kinomaniak.model.MovieCategory;
+import com.example.kinomaniak.service.AuthService;
+import com.example.kinomaniak.service.CashierService;
+import javafx.beans.Observable;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.Pane;
+import net.rgielen.fxweaver.core.FxWeaver;
+import net.rgielen.fxweaver.core.FxmlView;
+import org.springframework.beans.PropertyValue;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDate;
+import java.util.Calendar;
+import java.util.List;
+import java.util.Locale;
+import java.util.stream.Collectors;
+
+@Component
+@FxmlView("cashierMovieView.fxml")
+public class CashierMovieViewController {
+    private final CashierService cashierService;
+    @FXML
+    public TableView<Movie> moviesTable;
+    @FXML
+    public TableColumn<Movie,String> titleColumn;
+    @FXML
+    public TableColumn<Movie,String> directorColumn;
+    @FXML
+    public TableColumn<Movie,Integer> durationColumn;
+    @FXML
+    public TableColumn<Movie, LocalDate> premierDateColumn;
+    @FXML
+    public TableColumn<Movie,Integer> ageRestrictionColumn;
+    @FXML
+    public TextField searchTextField;
+    @FXML
+    public ComboBox<String> genreComboBox;
+    @FXML
+    public Button recommendedMoviesButton;
+    @FXML
+    public ComboBox<String> ageRestrictionComboBox;
+    @FXML
+    public Button showScreeningsButton;
+    @FXML
+    public Pane bottomPane;
+    @FXML
+    public Button hideBottomPaneButton;
+    @FXML
+    public TextArea descriptionTextArea;
+
+    private FxWeaver fxWeaver;
+
+    private ObservableList<Movie> movies;
+    private List<MovieCategory> movieCategories;
+
+    @Autowired
+    public CashierMovieViewController(FxWeaver fxWeaver, CashierService cashierService) {
+        this.fxWeaver = fxWeaver;
+        this.cashierService = cashierService;
+    }
+
+    @FXML
+    private void initialize(){
+
+        moviesTable.getSelectionModel().setSelectionMode(SelectionMode.SINGLE);
+
+        titleColumn.setCellValueFactory(new PropertyValueFactory<>("title"));
+        directorColumn.setCellValueFactory(new PropertyValueFactory<>("director"));
+        durationColumn.setCellValueFactory(new PropertyValueFactory<>("duration"));
+        premierDateColumn.setCellValueFactory(new PropertyValueFactory<>("premierDate"));
+        ageRestrictionColumn.setCellValueFactory(new PropertyValueFactory<>("ageRestriction"));
+
+        this.movies = cashierService.getMovies();
+        this.movieCategories = cashierService.getMovieCategories();
+        this.genreComboBox.getItems().add("not specified");
+        this.genreComboBox.getItems().addAll(movieCategories.stream().map(MovieCategory::getCategoryName).toList());
+        this.genreComboBox.setPromptText("not specified");
+        this.ageRestrictionComboBox.getItems().add("not specified");
+        this.ageRestrictionComboBox.getItems().addAll(movies.stream().map(Movie::getAgeRestriction).map(Object::toString).collect(Collectors.toSet()));
+        this.ageRestrictionComboBox.setPromptText("not specified");
+        moviesTable.setItems(movies);
+
+
+        searchTextField.textProperty().addListener(title-> moviesTable.setItems(filterResults()));
+        genreComboBox.valueProperty().addListener(genre-> moviesTable.setItems(filterResults()));
+        ageRestrictionComboBox.valueProperty().addListener(age->moviesTable.setItems(filterResults()));
+
+        moviesTable.getSelectionModel().selectedItemProperty().addListener((observable, oldValue, newValue) ->
+        {
+            if(oldValue == null){
+                moviesTable.setPrefHeight(moviesTable.getPrefHeight() - 100.0);
+                bottomPane.setPrefHeight(bottomPane.getPrefHeight()+ 100.0);
+                bottomPane.setVisible(true);
+            }
+            if(newValue == null){
+                moviesTable.setPrefHeight(moviesTable.getPrefHeight() + 100.0);
+                bottomPane.setPrefHeight(bottomPane.getPrefHeight() - 100.0);
+                bottomPane.setVisible(false);
+            }else{
+                descriptionTextArea.textProperty().bind(new SimpleStringProperty(newValue.getDescription()));
+            }
+
+
+
+
+        } );
+
+    }
+    private ObservableList<Movie> filterResults() {
+        return FXCollections.observableList(movies
+                .stream()
+                .filter(movie -> {
+                    String title = movie.getTitle().toLowerCase();
+                    return title.contains(searchTextField.getText());
+
+                }).filter(movie -> {
+                    if(genreComboBox.getValue() == null || genreComboBox.getValue().equals("not specified"))
+                        return true;
+                    return movie.getCategories()
+                            .stream()
+                            .map(MovieCategory::getCategoryName)
+                            .collect(Collectors.toSet())
+                            .contains(genreComboBox.getValue());
+                }).filter(movie -> {
+                    if(ageRestrictionComboBox.getValue() == null || ageRestrictionComboBox.getValue().equals("not specified"))
+                        return true;
+                    return movie.getAgeRestriction().toString().equals(ageRestrictionComboBox.getValue());
+
+                })
+                .collect(Collectors.toList())
+
+        );
+    }
+
+    private void resetFilters(){
+        genreComboBox.setValue("not specified");
+        searchTextField.setText("");
+        ageRestrictionComboBox.setValue("not specified");
+    }
+
+
+    public void showRecommendedMovies() {
+        removeSelection();
+        resetFilters();
+        moviesTable.setItems(FXCollections.observableList(movies
+                .stream()
+                .filter(movie -> movie.getPremierDate().isAfter(LocalDate.now().minusMonths(1)))
+                .collect(Collectors.toList()))
+        );
+
+    }
+
+    public void removeSelection() {
+        moviesTable.getSelectionModel().clearSelection();
+    }
+}
--- src/main/java/com/example/kinomaniak/controller/HomeController.java
+++ src/main/java/com/example/kinomaniak/controller/HomeController.java
@@ -20,6 +20,7 @@ import javafx.scene.control.RadioButton;
 import javafx.scene.control.ToggleGroup;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
@@ -222,6 +223,8 @@ public class HomeController {
 
     public void showFilmsCashier() {
         System.out.println("Films cashier");
+        GridPane cashierMoviePane = fxWeaver.loadView(CashierMovieViewController.class);
+        mainContent.setCenter(cashierMoviePane);
     }
 
     public void showFilmsManager() {
