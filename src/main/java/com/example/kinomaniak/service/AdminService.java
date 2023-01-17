d4768fd46b60674392b1309e5ee1a78e9b613796
Piotr Socała <piotrsocala@student.agh.edu.pl>
2023-01-18 00:16:25 +0100

    Unsuccessful E-mail sending development, should be working, but it's not :(

--- src/main/java/com/example/kinomaniak/service/AdminService.java
+++ src/main/java/com/example/kinomaniak/service/AdminService.java
@@ -10,6 +10,7 @@ import javafx.collections.ObservableList;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 
 @Service
@@ -18,43 +19,16 @@ public class AdminService {
     private final TicketRepository ticketRepository;
     private final FilmShowRepository filmShowRepository;
     private final RoleRepository roleRepository;
+    private final MailSendingService mailSendingService;
 
     public AdminService(EmployeeRepository employeeRepository, TicketRepository ticketRepository,
-                        FilmShowRepository filmShowRepository,RoleRepository roleRepository) {
+                        FilmShowRepository filmShowRepository,RoleRepository roleRepository,
+                        MailSendingService mailSendingService) {
         this.employeeRepository = employeeRepository;
         this.ticketRepository = ticketRepository;
         this.filmShowRepository = filmShowRepository;
         this.roleRepository = roleRepository;
-    }
-
-    public Employee addNewUser(Role role, String name, String surName, String mail, String password){
-        Employee employee = new Employee(role, name, surName, mail, password);
-        return employeeRepository.save(employee);
-    }
-
-    public Employee showEmployeeWhoSoldTheMostTickets(){
-        Optional<Ticket> ticketData = ticketRepository.findBestEmployee();
-        return ticketData.map(Ticket::getEmployee).orElse(null);
-    }
-
-//    public Integer numberOfCashier(){
-//        Optional<List<Employee>> cashiers = employeeRepository.findAllByRoleName("cashier");
-//        return cashiers.map(List::size).orElse(0);
-//    }
-
-//    public Integer numberOfManagers(){
-//        Optional<List<Employee>> managers = employeeRepository.findAllByRoleName("manager");
-//        return managers.map(List::size).orElse(0);
-//    }
-
-    public Hall showTheOftenChosenHallForEvents(){
-        Optional<FilmShow> oftenChosenHallData = filmShowRepository.findTheOftenChosenHallForEvents();
-        return oftenChosenHallData.map(FilmShow::getHall).orElse(null);
-    }
-
-    public Movie showMovieWithTheMostViewer(){
-        Optional<Ticket> ticketData = ticketRepository.findBestMovie();
-        return ticketData.map(ticket -> ticket.getFilmShow().getMovie()).orElse(null);
+        this.mailSendingService = mailSendingService;
     }
 
     public ObservableList<Employee> getEmployees(){
@@ -65,6 +39,19 @@ public class AdminService {
     }
 
     public void deleteEmployee(Employee employee) {
+        for (Employee employeeToNotify : getEmployees()){
+            if (Objects.equals(employeeToNotify.getMail(), employee.getMail())){
+                System.out.println("dupa");
+                mailSendingService.sendEmail(employee.getMail(), "Job dismissal",
+                        "Unfortunately, you have been fired from your position in KinoManiak Cinemas." +
+                                "You should've tried harder and now you're jobless ://");
+            } else {
+//                mailSendingService.sendEmail(employee.getMail(), "Job dismissal of your colleague",
+//                        "Unfortunately, one of your colleagues, " + employee.getName() + " " + employee.getSurName() +
+//                                ", have been fired from your position in KinoManiak Cinemas." +
+//                                "For now you are safe, but you had better work harder, or else...");
+            }
+        }
         employeeRepository.delete(employee);
     }
 
